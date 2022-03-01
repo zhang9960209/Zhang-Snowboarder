fbbf406fef4e22aacee3b1d4516a5d17d0797650
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-03-01 00:07:07 -0500

    Trigger to restart level

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+   void OnTriggerEnter2D(Collider2D other) 
+   {
+       if (other.tag == "Player")
+       {
+           Debug.Log("You Finished!");
+           SceneManager.LoadScene(0);
+       } 
+       
+   }
+}
